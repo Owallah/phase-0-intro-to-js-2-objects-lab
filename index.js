9876af878bda9a90076c7101a00c3194dcc20a7d
Johnson <mikesonowallah@gmail.com>
2023-03-12 13:30:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Johnson",
+    streetAddress: "Ngong rd",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return{
+        ...obj,
+        [key]:value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    // const newObj = {...obj};
+    
+    return delete {...obj},[key];
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
